5df0a014d6a735dabbe2d9c9abf08d2e1feb1e98
Carlos <carlemanx@gmail.com>
2017-02-13 17:55:29 -0600

    Mejoras de Goviz
    1. Integración de otras librerias para el tooltip de bubbletree.
    2. Json con los datos de ACT

---
+++ js/angular/app.js
@@ -0,0 +1,12 @@
+var app = angular.module("app", []);
+
+app.controller("MainCtrl", function($scope) {
+	
+});
+
+
+
+/* Jquery */
+$(document).ready(function(){
+   
+})
--- js/appBubbleTree.js
+++ js/appBubbleTree.js
@@ -1,31 +1,61 @@
-		$(function() {
-			var randomNames = ['Burgis', 'Pascal', 'Lysann', 'Theo', 'Julia', 'Barnabas', 'Immanuel', 'Marisa', 'Folker', 'Hadumod', 'Friedegunde', 'Marco', 'Otto', 'Sonnhardt', 'Arntraud', 'Andree', 'Wiltrudis', 'Astrid', 'Kathrein', 'Raoul', 'Vivien', 'Ole', 'Leo', 'Dankward', 'David', 'Ferfried', 'Sonngard', 'Fabio', 'Hansjakob', 'Huberta', 'Doro', 'Gordian', 'Sturmius', 'Sturmhard', 'Reintraud', 'Sabine', 'Georg', 'Sylvia', 'Ann', 'Editha', 'Gunhard', 'Etienne', 'Hildtraud', 'Noah', 'Margarete', 'Stilla', 'Brian', 'Pauline', 'Edgar', 'Kathrin'];
-			var nodeCount = 1;
-			function generateRandomData(node, level) {
-				if (!level) level = 1;
-				var numChildren = 10; //3+Math.round(Math.random()*6);
-				node.children = [];
-				var amount = node.amount;
-				for (var i=0; i<numChildren; i++) {
-					nodeCount ++;
-					var child = {
-						label: randomNames[Number(String(level-1)+String(i))],
-						amount: i+1 < numChildren ? amount*Math.random()*.6 : amount
-					};
-					if (level == 1) child.color = vis4color.fromHSL(i/numChildren*360, .7, .5).x;
-					if (level == 2) child.color = vis4color.fromHex(node.color).lightness('*'+(.5+Math.random()*.5)).x;
-					amount -= child.amount;
-					node.children.push(child);
-					if (level < 3) generateRandomData(child, level+1);
-				}
-				return node;
-			}
-			var data = generateRandomData({
-				label: 'Random Names, Random Amounts',
-				amount: 100000000
-			});
-			new BubbleTree({
-				data: data,
-				container: '.bubbletree'
-			});
-		});
+$(function() {
+	var arrarBubbleTree = [];
+	var formatNumber = function (n) {
+		var prefix = '';
+		if (n < 0) {
+			n = n * -1;
+			prefix = '-';
+		}
+		if (n >= 1000000000000) return prefix + Math.floor(n / 10000000000) / 100 + 'trillones';
+		if (n >= 1000000000) return prefix + Math.floor(n / 10000000) / 100 + ' billones';
+		if (n >= 1000000) return prefix + Math.floor(n / 10000) / 100 + ' millones';
+		if (n >= 1000) return prefix + Math.floor(n / 10) / 100 + ' mil';
+		else return prefix + n;
+	};
+
+	var initTooltip = function(node, domnode) {
+		domnode.setAttribute('tooltip', node.shortLabel+' &nbsp;<br /><big><b> $ '+node.famount+'</br></big>'+node.label);
+		$(domnode).tooltip({ delay: 10, bodyHandler: getTooltip });
+	};
+
+	var $tooltip = $('<div class="tooltip">Tooltip</div>');
+
+	$('.bubbletree').append($tooltip);
+	$tooltip.hide();
+
+
+	$.ajax({
+		url : './json/Division_Gastos_ACT.json',
+		data : {},
+		type : 'GET',
+		dataType : 'json',
+		success : function(json) {
+			           	new BubbleTree({
+                		autoColors: true,
+                		data: json,
+                		sortBy: 'amount',
+                		container: '.bubbletree',
+                		bubbleType: 'icon',
+                		maxNodesPerLevel: 100,
+                		tooltip: {
+                			qtip: true,
+                			delay: 200,
+                			content: function(node) {
+                				return [node.shortLabel,
+                				'<div class="desc"></div><div class="amount"> $ ' + node.amount +'<br><br>'+node.label +'</div>'];
+                			}
+                		}
+                	});
+		},
+		error : function(xhr, status) {
+			console.log('Disculpe, existió un problema');
+		},
+		complete : function(xhr, status) {
+			console.log('Petición realizada');
+		}
+	});
+});
+
+
+
+
